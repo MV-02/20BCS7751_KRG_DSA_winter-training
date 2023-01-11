21ec861031c598ad8b0714cf1e779e88d9aec7a4
mv-02 Laptop <mehul25169@gmail.com>
2023-01-11 14:37:16 +0530

    trie completed

--- Tries/trie.cpp
+++ Tries/trie.cpp
@@ -1,16 +1,82 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct trie
+const int siz = 26;
+
+struct TrieNode
 {
-    trie* node;
-    char val;
+    struct TrieNode *children[siz];
+
     bool end;
 };
 
-int main(){
-    trie * a=NULL;
-    
-    
-return 0;
+typedef struct TrieNode T;
+
+T *getNode(void)
+{
+    T *tmp = new TrieNode;
+
+    tmp->end = false;
+
+    for (int i = 0; i < siz; i++)
+        tmp->children[i] = NULL;
+
+    return tmp;
+}
+
+void insert(T *root, string key)
+{
+    T *temp = root;
+
+    for (int i = 0; i < key.length(); i++)
+    {
+        int index = key[i] - 'a';
+        if (!temp->children[index])
+            temp->children[index] = getNode();
+
+        temp = temp->children[index];
+    }
+
+    temp->end = true;
+}
+
+bool search(T *root, string key)
+{
+    T *temp = root;
+
+    for (int i = 0; i < key.length(); i++)
+    {
+        int index = key[i] - 'a';
+        if (!temp->children[index])
+            return false;
+
+        temp = temp->children[index];
+    }
+
+    return (temp->end);
+}
+
+int main()
+{
+    string keys[] = {"the", "a", "there",
+                     "answer", "any", "by",
+                     "bye", "their"};
+    int n = sizeof(keys) / sizeof(keys[0]);
+
+    T *root = getNode();
+
+    for (int i = 0; i < n; i++)
+        insert(root, keys[i]);
+
+    char output[][32] = {"Not present in trie", "Present in trie"};
+
+    cout << "the"
+         << " --- " << output[search(root, "the")] << endl;
+    cout << "these"
+         << " --- " << output[search(root, "these")] << endl;
+    cout << "their"
+         << " --- " << output[search(root, "their")] << endl;
+    cout << "thaw"
+         << " --- " << output[search(root, "thaw")] << endl;
+    return 0;
 }
